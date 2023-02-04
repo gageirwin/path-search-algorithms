0a0a468b434afbc9f69feaba80c8366923ac86f7
Gage Irwin <91108814+gageirwin@users.noreply.github.com>
2023-02-03 20:21:17 -0600

    Update maze.py
    added Dijkstra algorithm. added clear search to reuse same maze. changed how tile switching is handled.

--- src/maze.py
+++ src/maze.py
@@ -1,7 +1,7 @@
 import pygame
 from src.settings import *
 from src.tile import *
-from src.algorithms.a_star import a_star
+from src.algorithms import a_star, Dijkstra
 
 class Maze:
 
@@ -14,6 +14,46 @@ class Maze:
 
     def reset(self):
         self.maze = [[ Tile((col, row), TileType.EMPTY) for col in range(MAZE_WIDTH) ] for row in range(MAZE_HEIGHT)]
+        self.start = None
+        self.end = None
+
+    def clear_search(self):
+        for row in self.maze:
+            for tile in row:
+                if tile.type in {TileType.SEARCHED,TileType.PATH}:
+                    tile.change_type(TileType.EMPTY)        
+
+    def remove_start(self):
+        for row in self.maze:
+            for tile in row:
+                if tile.type == TileType.START:
+                    tile.change_type(TileType.EMPTY)
+                    self.start = None
+
+    def remove_end(self):
+        for row in self.maze:
+            for tile in row:
+                if tile.type == TileType.END:
+                    tile.change_type(TileType.EMPTY)
+                    self.end = None             
+
+    def draw(self):
+        self.display_surface.fill('white')
+        for row in self.maze:
+            for tile in row:
+                action = tile.draw(self.display_surface)
+                if action:
+                    self.clear_search()
+                if action == 'm0':
+                    tile.flip_type(TileType.WALL)
+                elif action == 'm1':
+                    self.remove_start()
+                    tile.change_type(TileType.START)
+                    self.start = tile.pos
+                elif action == 'm2':
+                    self.remove_end()
+                    tile.change_type(TileType.END)
+                    self.end = tile.pos
 
     def input(self):
         keys = pygame.key.get_pressed()
@@ -21,7 +61,18 @@ class Maze:
             if self.key_pressed == False:
                 self.key_pressed = True
                 if self.start and self.end:
+                    self.clear_search()
                     a_star(self.maze, self.start, self.end, self.draw)
+        elif keys[pygame.K_d]:
+            if self.key_pressed == False:
+                self.key_pressed = True
+                if self.start and self.end:
+                    self.clear_search()
+                    Dijkstra(self.maze, self.start, self.end, self.draw)
+        elif keys[pygame.K_c]:
+            if self.key_pressed == False:
+                self.key_pressed = True
+                self.clear_search()
         elif keys[pygame.K_r]:
             if self.key_pressed == False:
                 self.key_pressed = True
@@ -29,39 +80,6 @@ class Maze:
         else:
             self.key_pressed = False
 
-    def clear_type(self, type):
-        for row in self.maze:
-            for col in row:
-                if col.type == type:
-                    col.change_type(TileType.EMPTY)
-
-    def draw(self):
-        self.display_surface.fill('white')
-        for row in self.maze:
-            for col in row:
-                action = col.draw(self.display_surface)
-                if action == 'm0':
-                    if col.type == TileType.WALL:
-                        col.change_type(TileType.EMPTY)
-                    else:
-                        col.change_type(TileType.WALL)
-                elif action == 'm1':
-                    if col.type == TileType.START:
-                        col.change_type(TileType.EMPTY)
-                        self.start = None
-                    else:
-                        self.clear_type(TileType.START)
-                        col.change_type(TileType.START)
-                        self.start = col.pos
-                elif action == 'm2':
-                    if col.type == TileType.END:
-                        col.change_type(TileType.EMPTY)
-                        self.end = None
-                    else:
-                        self.clear_type(TileType.END)
-                        col.change_type(TileType.END)
-                        self.end = col.pos
-        
     def run(self):
         self.draw()
         self.input()
